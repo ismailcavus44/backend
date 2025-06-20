b56440f03be0f8f6f74e60b6ae5a6734622e89c8
ismailcavus44 <asdsadsdsff@gmail.com>
2025-06-20 10:25:58 +0300

    fix: properly rename expiredtoken to lowercase

--- models/expiredtoken_TEMP.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ExpiredTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true, unique: true },
-  expiredAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('ExpiredToken', ExpiredTokenSchema);
